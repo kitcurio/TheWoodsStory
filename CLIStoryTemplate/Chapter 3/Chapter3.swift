13341f1a9137f99a112e645e5310a01c4ede99fd
Gwendolyn Wilson <wilson87gw@gmail.com>
2023-10-30 11:00:13 -0400

    Added chipmunk scene

--- CLIStoryTemplate/Chapter 3/Chapter3.swift
+++ CLIStoryTemplate/Chapter 3/Chapter3.swift
@@ -7,4 +7,21 @@ import Foundation
 
 func chapterThree() {
     // Your portion of the story goes here
+    print("Declaring Chapter 3")
+    print("She is forced to be brave enough to take the 2day dangerous path, which contains the most potent herbs. These herbs can be used as medicine to heal her mother. Initially nothing about this path seemed threatening. She felt at ease, immersed in the beauty of the lush forest. About an hour in, her perception started change. The behavior of the creatures and plants got a bit alarming. There was a cute chipmunk with a potion stand, so she stopped to inquire if the chipmunk knew about the wizard.")
+    
+    let scene1 = "The chipmunk leads her all the way. Wow"
+    let scene2 = "So he chases her all the way to the wizard. Cool. "
+    
+    var potion = ["Mystical", "Magical", "Random"]
+    
+    func chipmunkScene() {
+        let scenes = [scene1, scene2]
+        let randomNum = Int.random(in: 0..<scenes.count)
+        
+        print(scenes[randomNum])
+    }
+    chipmunkScene()
 }
+
+// Expansive Woods, Elusive Problem
